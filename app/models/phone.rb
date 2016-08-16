ce968b1cd1c21ef2ac930b848046c586a2b0fd62
John Owen <johnowenatala@gmail.com>
2016-08-16 17:57:14 -0300

    Base for phone controller

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,5 +1,7 @@
 class Phone < ActiveRecord::Base
   belongs_to :brand
 
+  enum :compatibility => { full: 1, good: 2, medium: 3, poor: 4, unknown: nil }
+
   validates :brand_id, :model, presence: true
 end
